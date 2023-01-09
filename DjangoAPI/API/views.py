c4c6d0fc006f85ce6e9492d77e8e51b497cf44d2
RitikChahar <ritickchahar@gmail.com>
2023-01-09 22:49:15 +0530

    created DjangoAPI

---
+++ DjangoAPI/API/views.py
@@ -0,0 +1,14 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+import json
+
+@csrf_exempt
+def name(request):
+    if (request.method=="POST"):
+        request_data=json.loads(request.body)
+        Name = request_data['name']
+        LastName= request_data['lastname']
+        fullName = f"Your name is {Name} {LastName}"
+        return JsonResponse ({"Answer":fullName})
+    else:
+        return HttpResponse("Method Not Allowed")   
